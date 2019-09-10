5b34a4168f0dc1781d54e4d4fe8ab8b3e563b1c5
xxAffable <318178142@qq.com>
2019-09-10 14:51:13 +0800

    推荐可能的好友 Spark实现

---
+++ src/main/java/com/gjxx/java/spark/learn/friendrecommendation/SparkFriendRecommendation.java
@@ -0,0 +1,10 @@
+package com.gjxx.java.spark.learn.friendrecommendation;
+
+/**
+ * @Description TODO
+ * @Author Sxs
+ * @Date 2019/9/10 14:50
+ * @Version 1.0
+ */
+public class SparkFriendRecommendation {
+}
